a889967f4164e469cfb07120d051a96f10b61bd1
Christopher Pace <Christopher_pace@ymail.com>
2018-10-11 00:10:22 -0500

    Finished

--- timecard_project/timesheetApp/views.py
+++ timecard_project/timesheetApp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .models import Timesheet
-
 def index(request):
     timesheet_list = Timesheet.objects.all()
     context = {'timesheet_list' : timesheet_list}
